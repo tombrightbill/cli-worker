b3fdfed6af82c0f8ec0e1fc4649adba84059d61b
Tom Brightbill <tbrightbill@cloudflare.com>
2018-12-11 11:57:30 -0800

    new commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 cf worker upload-worker --zone-id $ZONE --script @$SCRIPT
 
-cf worker list-worker-scripts --zone-id $ZONE
+cf worker list-worker-scripts --organization-id b191549164778b850e88b43187c1bd21
